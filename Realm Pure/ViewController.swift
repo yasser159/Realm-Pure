08087cda627d18663ec31f67b4070e2659a4792a
Yasser Hajlaoui <yasser159@gmail.com>
2022-03-05 19:39:17 -0800

    Initial Commit

---
+++ Realm Pure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realm Pure
+//
+//  Created by Yasser Hajlaoui on 3/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
